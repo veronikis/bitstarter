152310de4183267c70d322e8034b354b0601a37f
Spyros Veronikis <spyros.veronikis@gmail.com>
2013-07-09 19:04:37 +0000

    Modified web.js to read conent from file

--- web.js
+++ web.js
@@ -1,12 +1,26 @@
+#!/usr/bin/env node
+
 var express = require('express');
+var app= express.createServer(express.logger());
 
-var app = express.createServer(express.logger());
+app.get('/', 
+  function(request, response){
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+     var fs = require('fs');
+     var buffer = new Buffer(50);
+     // var content = fs.readFileSync("index.html", "utf-8");
+     buffer = fs.readFileSync("index.html");
+     var content = buffer.toString("utf-8")
+
+     response.send(content);
+  }	
+);
+
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, 
+  function(){
+    console.log("Listening on port " + port);
+}
+
+);
